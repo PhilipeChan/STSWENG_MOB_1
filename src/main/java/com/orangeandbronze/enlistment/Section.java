11d30265f28cac71d95fb2522761875ff96f84c2
PhilipeChan <104419764+PhilipeChan@users.noreply.github.com>
2023-01-24 18:02:47 +0800

    1/24/2023 Foundation for Student and Section classes

---
+++ src/main/java/com/orangeandbronze/enlistment/Section.java
@@ -0,0 +1,34 @@
+package com.orangeandbronze.enlistment;
+
+import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.Validate.*;
+
+import java.util.Objects;
+
+class Section {
+    private final String sectionId;
+
+    Section(String sectionId) {
+        notBlank(sectionId);
+        isTrue(isAlphanumeric(sectionId), "sectionId must be alphanumeric, was: " + sectionId);
+        this.sectionId = sectionId;
+    }
+
+    @Override
+    public String toString() {
+        return sectionId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Section section = (Section) o;
+        return sectionId.equals(section.sectionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sectionId);
+    }
+}
